2941c5f27e399ba468d9bd2f734792cf742990eb
Tactition <136788350+Tactition@users.noreply.github.com>
2025-03-21 19:57:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,7 +67,7 @@ async def start():
     if ON_HEROKU:
         asyncio.create_task(ping_server())
     me = await StreamBot.get_me()
-    tz = pytz.timezone('Asia/Kashmir')
+    tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
